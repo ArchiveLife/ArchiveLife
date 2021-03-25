623be6ccfd8a427c6d69611df1e0b5f89a027d4c
Theo Sun <theo.sun@sap.com>
2021-03-25 13:16:38 +0800

    chore(dep): added

--- go.mod
+++ go.mod
@@ -1,12 +1,13 @@
-module github.com/Soontao/go-project-template
+module github.com/ArchiveLife/ui
 
 go 1.14
 
 require (
+	fyne.io/fyne/v2 v2.0.1
 	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/stretchr/testify v1.7.0
-	github.com/takama/daemon v1.0.0
-	github.com/urfave/cli v1.22.5
+	github.com/takama/daemon v1.0.0 // indirect
+	github.com/urfave/cli v1.22.5 // indirect
 	golang.org/x/sys v0.0.0-20210320140829-1e4c9ba3b0c4 // indirect
 )
--- main/main.go
+++ main/main.go
@@ -1,48 +1,22 @@
 package main
 
 import (
-	"log"
-	"os"
-	"sort"
-
-	"github.com/urfave/cli"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
 )
 
-// Version string, in release version
-// This variable will be overwrited by complier
-var Version = "SNAPSHOT"
-
-// AppName of this application
-var AppName = "CommandLineToolUtil"
-
-// AppUsage of this application
-var AppUsage = "A Command Line Tool"
-
 func main() {
-	app := cli.NewApp()
-	app.Version = Version
-	app.Name = AppName
-	app.Usage = AppUsage
-	app.Flags = options
-	app.EnableBashCompletion = true
+	a := app.New()
+	w := a.NewWindow("Hello")
 
-	commonCommands := []cli.Command{
-		commandEntry,
-	}
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(container.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
 
-	daemonCommands, err := createDaemonCommands(AppName, AppUsage)
-
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	app.Commands = append(commonCommands, daemonCommands...)
-
-	sort.Sort(cli.CommandsByName(app.Commands))
-
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
-	}
-
+	w.ShowAndRun()
 }
